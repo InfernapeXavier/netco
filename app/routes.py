060603df852690c5928857ece548d4afd9a1d372
Restyled.io <commits@restyled.io>
2020-03-14 03:48:13 +0000

    Restyled by reorder-python-imports

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,32 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
-from flask_login import current_user, login_user
-from app.models import User
-from flask_login import logout_user
-from flask_login import login_required
-from flask import request
-from werkzeug.urls import url_parse
-from app import db
-from app.forms import RegistrationForm
 from datetime import datetime
-from app.forms import EditProfileForm
-from app.forms import PostEventForm
-from app.forms import ManageForm
-from app.models import Post
-from app.forms import ResetPasswordRequestForm
-from app.email import send_password_reset_email
-from app.forms import ResetPasswordForm
+
+from flask import flash
 from flask import g
-from flask_babel import _, get_locale
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask_babel import _
+from flask_babel import get_locale
+from flask_login import current_user
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import logout_user
 from guess_language import guess_language
+from werkzeug.urls import url_parse
+
+from app import app
+from app import db
+from app.email import send_password_reset_email
+from app.forms import EditProfileForm
+from app.forms import LoginForm
+from app.forms import ManageForm
+from app.forms import PostEventForm
+from app.forms import RegistrationForm
+from app.forms import ResetPasswordForm
+from app.forms import ResetPasswordRequestForm
+from app.models import Post
+from app.models import User
 
 
 @app.before_request
